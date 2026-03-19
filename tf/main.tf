0b030991cec5ba62d6afa55ae5b65531598f57a5
Premansh-Pareek <premansh.pareek@symplora.com>
2026-03-19 13:17:26 +0530

    Update .gitignore and Terraform configuration; remove lock file and add default variables

--- tf/main.tf
+++ tf/main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # version = "~> 3.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
+  profile = var.profile
   region = var.region
 }
 
--- tf/vars.tf
+++ tf/vars.tf
@@ -1,13 +1,21 @@
 variable "package_bucket" {
   type = string
+  default = "sypi-storage"
   description = "Name of the S3 bucket where pypi packages are to be stored"
 }
 
 variable "region" {
   type = string
+  default = "ap-south-1"
 }
 
 variable "log_group" {
   type = string
+  default = "sypi"
   description = "CloudWatch log group for access logs"
+}
+
+variable "profile" {
+  type = string
+  default = "symplora-prod"
 }
